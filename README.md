5ed104669d4f06ec40115e1d1bb929157d925602
wandalen <wandalen.me@gmail.com>
2016-10-12 12:01:37 +0400

    publish

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wEventHandler
 Module in JavaScript that provides an easy way to manage events between objects.
+
--- sample/Sample1.js
+++ sample/Sample1.js
@@ -1,6 +1,83 @@
+( function _Sample_s_( ) {
 
+'use strict';
+
+// dependencies
 
 if( typeof module !== 'undefined' )
-require( 'wEventHandler' );
+{
+
+  require( 'wEventHandler' );
+
+}
+
+// constructor
 
 var _ = wTools;
+var Parent = null;
+var Self = function Sample( o )
+{
+  if( !( this instanceof Self ) )
+  return new( _.routineJoin( Self, Self, arguments ) );
+  return Self.prototype.init.apply( this,arguments );
+}
+
+// --
+// inter
+// --
+
+var init = function()
+{
+  var self = this;
+
+}
+
+//
+
+var event1 = function()
+{
+  var self = this;
+
+  self.eventHandle( 'event1' );
+
+}
+
+// --
+// proto
+// --
+
+var Proto =
+{
+
+  init : init,
+  constructor : Self,
+
+};
+
+_.protoMake
+({
+  constructor : Self,
+  parent : Parent,
+  extend : Proto,
+});
+
+wEventHandler.mixin( Self );
+
+_global_.Sample = Self;
+
+// make an instance
+
+sample = new Sample;
+
+sample.on( 'event1',function( e ) {
+  console.log( e )
+});
+
+sample.on( 'event2',function( e ) {
+  console.log( e )
+});
+
+sample.event1();
+sample.eventHandle( 'event2' );
+
+})();

535efb5f5940d9f6e84fa7c58d71201579cb4ed5
Wandalen <wandalen.me@gmail.com>
2018-06-13 22:10:25 +0300

    sync

--- staging/dwtools/abase/mixin.test/EventHandler.test.s
+++ staging/dwtools/abase/mixin.test/EventHandler.test.s
@@ -280,7 +280,7 @@ function eventWaitFor( test )
 }
 
 // --
-// proto
+// define class
 // --
 
 var Self =
@@ -301,7 +301,7 @@ var Self =
 
 //
 
-Self = wTestSuit( Self );
+Self = wTestSuite( Self );
 if( typeof module !== 'undefined' && !module.parent )
 _.Tester.test( Self.name );
 
